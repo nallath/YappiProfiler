64b86c8a25073e9b417ed00046b037e4f58d0b75
Jaime van Kessel <nallath@gmail.com>
2019-11-01 16:07:57 +0100

    Add some messages to make it clearer that it's doing something

--- YappiProfiler.py
+++ YappiProfiler.py
@@ -10,12 +10,14 @@ class YappiProfiler(Extension):
         self.addMenuItem("Start", self.startProfiling)
         self.addMenuItem("Stop", self.stopProfiling)
 
-        self._profiling_stopped_message = Message("Results were saved to curaProfile.out")
-
+        self._profiling_stopped_message = Message("Results were saved to curaProfile.out", title = "YappiProfiler")
+        self._profiling_started_message = Message("Started profiling...", title = "YappiProfiler")
 
     def startProfiling(self):
         if not self._is_profiling:
             self._is_profiling = True
+            self._profiling_stopped_message.hide()
+            self._profiling_started_message.show()
             yappi.start()
 
     def stopProfiling(self):
@@ -25,6 +27,6 @@ class YappiProfiler(Extension):
 
             func_stats = yappi.get_func_stats()
             func_stats.save("curaProfile.out", "CALLGRIND")
-            self._profiling_stopped_message.show()
-            yappi.stop()
             yappi.clear_stats()
+            self._profiling_started_message.hide()
+            self._profiling_stopped_message.show()
